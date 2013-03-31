f055aa0d9c7ab34b9390a4b6870df496b0d73b58
Jim Cushing <jimc@thelistinc.com>
2013-03-31 18:24:25 -0400

    initial commit

---
+++ apply.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+usage() {
+	echo "usage: $(basename $0) [-f] repo [repo ...]"
+	echo 
+	echo "Options:"
+	echo "    -f: Overwrite existing commit-msg and prepare-commit-msg"
+	echo "        files if they exist."
+}
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+if [[ "$1" = "-f" ]]; then
+	LN_OPTS="-f"
+	shift
+fi
+
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
+fi
+
+for dest in $@; do
+	if [ ! -d $dest ]; then
+		echo "$dest does not exist or is not a directory."
+		continue
+	fi
+
+	if [ ! -d $dest/.git ]; then
+		echo "$dest is not a git repository."
+		continue
+	fi
+
+	if [[ -z "$LN_OPTS" && -f $dest/.git/hooks/commit-msg ]]; then
+		echo "$dest: commit-msg hook already exists. Use '$(basename $0) -f ...' to overwrite."
+	else
+		ln -s $LN_OPTS $SCRIPTPATH/hooks/commit-msg $dest/.git/hooks/commit-msg
+	fi
+
+	if [[ -z "$LN_OPTS" && -f $dest/.git/hooks/prepare-commit-msg ]]; then
+		echo "$dest: prepare-commit-msg hook already exists. Use '$(basename $0) -f ...' to overwrite."
+	else
+		ln -s $LN_OPTS $SCRIPTPATH/hooks/commit-msg $dest/.git/hooks/prepare-commit-msg
+	fi
+done
+
